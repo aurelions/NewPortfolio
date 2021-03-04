1b4ccdc7feacb00779c5928d9958a054555e2aea
Conary36 <crossingwaters@icloud.com>
2021-03-03 22:34:42 -0500

    testing background

--- my_folio/src/components/About.js
+++ my_folio/src/components/About.js
@@ -4,9 +4,12 @@ import {Link} from 'react-router-dom';
 export default function About(){
 
 return (
-    <div>
+    <div id='title'>
          <h1>About</h1>
-        <Link to='/projects'>
+         <span>Programming a new world one project at a time, this is my passion.</span><br/> 
+         <span>My creative abilities come to life</span>
+
+        <Link id='p_btn' to='/projects'>
             <button>Projects</button>
         </Link>
     </div>
