2e88fb47b4b3d73b9ec9f549170be28671034045
Conary36 <crossingwaters@icloud.com>
2021-05-15 22:15:04 -0400

    homepage styling changes...

--- my_folio/src/components/Home.js
+++ my_folio/src/components/Home.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import '../styles/home.scss'
+import Image from 'react-bootstrap/Image'
 
 export default function Home(){
     return (
-        <div id="title">
-
-            <span>Hello, my name is Conary,</span><br/>
-            <span>I am a Full-Stack Web Developer</span><br/>
-        </div>
+    <div className='home_content'>
+        <section className='home_img'>
+             <Image fluid/>
+        </section>
+        <section className='bio'>
+        <span>Hello, my name is Conary,
+            I am a Full-Stack Web Developer.
+            What can we create together?
+        </span><br/>
+        </section>
+     </div>
         
         
         
