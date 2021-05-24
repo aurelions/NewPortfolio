9da4a7c20c25185ca4428d3ea40f4d9582a2b0d2
Conary36 <crossingwaters@icloud.com>
2021-05-24 00:46:33 -0400

    swiperjs implamented

--- my_folio/src/components/Projects.js
+++ my_folio/src/components/Projects.js
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from "react";
-// import sanityClient from '../client.js'
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper.min.css";
+import "swiper/components/pagination/pagination.min.css";
+import SwiperCore, { Mousewheel, Pagination } from "swiper/core";
 import firebase from "../firebase";
 import { Card, Image, Button, ButtonGroup } from "react-bootstrap";
 import '../styles/projects.scss'
 // import {Link} from 'react-router-dom'
+
+
 
 export default function Projects() {
   const [projectData, setProjectData] = useState([]);
@@ -16,6 +21,24 @@ export default function Projects() {
     setProjectData(projects);
     setLoading(false);
   };
+
+
+  SwiperCore.use([Mousewheel, Pagination]);
+
+
+  // const swiper = (".projects-slider", {
+  //   spaceBetween: 30,
+  //   effect: "fade",
+  //   loop: true,
+  //   mousewheel: {
+  //     invert: false,
+  //   },
+  //   // autoHeight: true,
+  //   pagination: {
+  //     el: ".projects-slider__pagination",
+  //     clickable: true,
+  //   },
+  // });
 
   // eslint-disable-next-line react-hooks/rules-of-hooks
   useEffect(() => {
@@ -30,37 +53,55 @@ export default function Projects() {
   console.log("projectData==>>", projectData);
 
   return (
-    <div className="projects-slider">
+    <Swiper
+      direction={"vertical"}
+      // slidesPerView={1}
+      effect={"fade"}
+      loop={true}
+      spaceBetween={30}
+      mousewheel={{invert: false}}
+      pagination={{
+        el: "projects-slider__pagination",
+        clickable: true,
+      }}
+     
+    >
       <div className="projects-slider__wrp swiper-wrapper">
         <div className="projects-slider__item swiper-slide">
           {loading ? <h1>Loading...</h1> : null}
           {projectData.map((proj, i) => (
-            <Card key={i} className="card-slider">
-              {proj.projectImg && <span className="img-span">
-                <Image
-                  src={require(`../images/${proj.projectImg}`).default}
-                  alt={proj.alt}
-                  fluid
-                />
-              </span>}
-              <div className="projects-slider__content">
-                <div className="projects-slider__title">{proj.projectName}</div>
-                <div className="projects-slider__text">{proj.description}</div>
-              </div>
-        
-              <a
-                href={proj.projectUrl}
-                className="project-slider__button"
-                target="_blank"
-              >
-                Open Link
-              </a>
-              
-              
-            </Card>
+            <SwiperSlide>
+              <Card key={i} className="">
+                {proj.projectImg && (
+                  <span className="img-span">
+                    <Image
+                      src={require(`../images/${proj.projectImg}`).default}
+                      alt={proj.alt}
+                      fluid
+                    />
+                  </span>
+                )}
+                <div className="projects-slider__content">
+                  <div className="projects-slider__title">
+                    {proj.projectName}
+                  </div>
+                  <div className="projects-slider__text">
+                    {proj.description}
+                  </div>
+                </div>
+
+                <a
+                  href={proj.projectUrl}
+                  className="project-slider__button"
+                  target="_blank"
+                >
+                  Open Link
+                </a>
+              </Card>
+            </SwiperSlide>
           ))}
         </div>
       </div>
-    </div>
+    </Swiper>
   );
 }
